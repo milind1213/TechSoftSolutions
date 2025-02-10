a4920a47c29e9bb2115b5964364f148cf99de3e4
milind1213 <gmilind12@gmail.com>
2025-02-10 23:16:45 +0530

    First commit

---
+++ src/main/java/TechSoftSolutions/TechSoftSolutions/App.java
@@ -0,0 +1,10 @@
+package TechSoftSolutions.TechSoftSolutions;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
